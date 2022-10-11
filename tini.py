d829caccf28dcbcc07f37f0c5d0bd93098bc84d9
ryancarter <ryan.c4rter@gmail.com>
2022-10-11 15:32:26 +1300

    fix: optimizations and instructions added

--- tini.py
+++ tini.py
@@ -4,38 +4,43 @@ import PySimpleGUI as sg
 import os
 import spotipy
 import string
+import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 
+""" INSTRUCTIONS
+1. Open Google Cloud Console: https://console.cloud.google.com/
+2. Make a new project, call it whatever
+3. Select this new project
+4. Search for "YouTube Data API v3" and enable it
+5. Search for "Credentials"
+6. Create Credentials -> API key
+7. Copy the key, replace line 23's "<API key>" with the key
+8. Run
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+There is an upper limit of (30,000?) daily calls you can make on free keys
+"""
 
 def get_youtube_api():
-    keys = ['AIzaSyCdz8nt6AcSFb_wKwtqIQvn213-uGxvwmM']#'AIzaSyDxqg_HAChixK8D1Z-YBGW-9T7MbXyttIo', 'AIzaSyCuxbIeHbltns2KJSAh-x_PN-HyBcw0xX4', 'AIzaSyCD9KPX9ahqgRMCgi8l0KZE5MOELwpodk4', 'AIzaSyDk6md1mOYxZPbmWfz_7P1daGOo6-8qk44', 'AIzaSyBcDdYe12LIAF0TYOAVVOmfxCx5wsW2FjM']
-    #keys = ["<key1>", "<key2>", "<etc>"]
-    invalid_key = True
-    i = 0
-    print("getting valid api key..")
-    while invalid_key:
-        try:
-            youtube_api = Api(api_key=keys[i])
-            search = youtube_api.search_by_keywords(q="", search_type=["video"], count=1, limit=1)
-            print(bcolors.OKGREEN + keys[i]+ " hasn't hit it's daily call limit yet :)" + bcolors.ENDC)
-            return youtube_api
-        except:
-            print(bcolors.FAIL +keys[i] + " hit it's daily call limit :(" + bcolors.ENDC)
-            i+=1
-            if i == len(keys):
-                return
-api = get_youtube_api()
+    key = "<API key>"
+    print(Colors.BLUE + "Getting valid api key.." + Colors.END)
+    try:
+        api = Api(api_key=key)
+        search = api.search_by_keywords(q="", search_type=["video"], count=1, limit=1)
+        print(Colors.GREEN + "Youtube API initialised successfully" + Colors.END)
+        return api
+    except Exception as e:
+        print(Colors.FAIL + str(e) + Colors.END)
+    return None
+
+class Colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def get_all_playlists(username):
     all_playlist_track_names = []
@@ -68,7 +73,6 @@ def get_one_playlist(playlist_link):
             track_artist = track["track"]["artists"][0]["name"]
             result = [track_name, track_artist]
             tracks.append(result)
-
     return tracks
 
 def download_ytvid_as_mp3(video_url, track, download_location):
@@ -86,21 +90,22 @@ def download_ytvid_as_mp3(video_url, track, download_location):
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-    print("Download complete... {}".format(filename))
+    print(Colors.GREEN + "Download complete... {}".format(filename) + Colors.END)
 
-def download(track_names, download_location, api, is_search = False,):
+def download(track_names, download_location, youtube_api, is_search = False):
     for track in track_names:
         search_query = ""
         count_limit = 1
         try:
             search_query = track[0]+' - '+track[1]
         except IndexError:
-            print("Please enter search in the format <song-name>:<artist>")
+            print(Colors.FAIL + "Please enter search in the format <song-name>:<artist>" + Colors.END)
             return
         if is_search:
             count_limit = 100
             search_query += " audio"
-        search = api.search_by_keywords(q=search_query, search_type=["video"], count = count_limit, limit = count_limit)
+            print(Colors.BLUE + "Searching Youtube for: " + search_query + Colors.END)
+        search = youtube_api.search_by_keywords(q=search_query, search_type=["video"], count = count_limit, limit = count_limit)
         for search_result in search.items:
             try:
                 download_ytvid_as_mp3(search_result.id.videoId, track, download_location)
@@ -110,37 +115,42 @@ def download(track_names, download_location, api, is_search = False,):
                 print(message)
                 print(exc_info)
 
+# MAIN
+youtube_api = get_youtube_api()
+if youtube_api == None:
+    sys.exit()
+
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-layout = [  [sg.Text('Search for a single song like this, with a colon in the middle <song-name>:<artist>:'), sg.InputText(key='search')],
+layout = [  [sg.Text('Search for a single song in format <song-name>:<artist>:'), sg.InputText(key='search')],
             [sg.Text('OR input a Spotify Playlist URL for one playlist:'), sg.InputText(key='url')],
-            [sg.Text('OR a username for all their playlists:'), sg.InputText(key='username')],
+            [sg.Text('OR a Spotify username for all their playlists:'), sg.InputText(key='username')],
             [sg.Text('Current Folder'), sg.FolderBrowse('Browse Download Location', key='download_location')],
             [sg.Button('Download')],
             [sg.Text('', key='result_label')]]
-
 
 window = sg.Window('TINI', layout).Finalize()
 window.Maximize()
 
 while True:
     event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        sys.exit()
     if event in (None, 'Download'):
-        download_location = values['download_location'] if values['download_location'] != "" else '.'
-        if values['url'] != "":
-            print('Downloading all tracks on that playlist. Please wait..')
-            track_names = get_one_playlist(values['url'])
-            download(track_names, download_location, api)
-            break
-        elif values['username'] != "":
-            print('Downloading all playlists by that user. Please wait..')
-            track_names = get_all_playlists(values['username'])
-            download(track_names, download_location, api)
-            break
-        elif values['search'] != "":
-            print('Downloading the song based on a search. Please wait..')
-            track_names = [values['search'].split(":")]
-            download(track_names, download_location, api, True)
-            break
-
+        try:
+            download_location = values['download_location'] if values['download_location'] != "" else '.'
+            if values['url'] != "":
+                print(Colors.BLUE + 'Downloading all tracks on that playlist. Please wait..' + Colors.END)
+                track_names = get_one_playlist(values['url'])
+                download(track_names, download_location, youtube_api)
+            elif values['username'] != "":
+                print('Downloading all playlists by that user. Please wait..')
+                track_names = get_all_playlists(values['username'])
+                download(track_names, download_location, youtube_api)
+            elif values['search'] != "":
+                print('Downloading the song based on a search. Please wait..')
+                track_names = [values['search'].split(":")]
+                download(track_names, download_location, youtube_api, True)
+        except Exception as e:
+            print(Colors.FAIL + str(e) + Colors.END)
 window.close()
